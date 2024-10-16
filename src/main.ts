1595b436270ee67acc417cef8d858fb3dc1123f7
jamshidbekml <jamshidbekml@gmail.com>
2024-10-16 13:05:01 +0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -55,7 +55,8 @@ async function bootstrap() {
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365,
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        // secure: process.env.NODE_ENV === 'production',
+        secure: false,
         sameSite: 'none',
       },
     }),
