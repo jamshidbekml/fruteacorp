86ac1966da194199febd77d4759329dc6ce37329
jamshidbekml <jamshidbekml@gmail.com>
2024-10-16 13:29:39 +0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,7 @@ async function bootstrap() {
         // httpOnly: true,
         // secure: process.env.NODE_ENV === 'production',
         secure: false,
-        sameSite: 'none',
+        // sameSite: 'none',
       },
     }),
   );
