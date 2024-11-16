dbfd3f45f81b2e010a2e48d31ef5f6f2f93923ff
jamshidbekml <jamshidbekml@gmail.com>
2024-11-16 15:53:54 +0500

    initial commit

--- src/api/exel/exel.controller.ts
+++ src/api/exel/exel.controller.ts
@@ -1,34 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ExelService } from './exel.service';
-import { CreateExelDto } from './dto/create-exel.dto';
-import { UpdateExelDto } from './dto/update-exel.dto';
 
 @Controller('exel')
 export class ExelController {
   constructor(private readonly exelService: ExelService) {}
-
-  @Post()
-  create(@Body() createExelDto: CreateExelDto) {
-    return this.exelService.create(createExelDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.exelService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.exelService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateExelDto: UpdateExelDto) {
-    return this.exelService.update(+id, updateExelDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.exelService.remove(+id);
-  }
 }
